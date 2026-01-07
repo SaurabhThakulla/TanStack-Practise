3751c972054b13d6f43ec81355841c404f9420db
Saurabh <sauravthakulla683@gmail.com>
2026-01-07 23:00:41 +0530

    ADD :  Query Added

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,3 +1,26 @@
-export default function App() {
-  return <h1>TanStackQuery Practise ✅</h1>;
+import { useQuery } from "@tanstack/react-query";
+function fetchPosts() {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+    res.json()
+  );
 }
+
+export default function App() {
+   const { data, isLoading, isError } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>Error fetching posts</h2>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {data.slice(0, 10).map((post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
+    </div>
+  );
+}
+
