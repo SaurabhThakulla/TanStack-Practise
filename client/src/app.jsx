ec5efec93db6dddf15aa944f260629b4d0227501
Saurabh <sauravthakulla683@gmail.com>
2026-01-30 22:45:29 +0530

    add : update query fn

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,25 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import Posted from "./api/api"
 
 export default function App() {
-  const {data} = useQuery({
-    queryKey: ['post'],
+  const { data } = useQuery({
+    queryKey: ['posts'],
     queryFn: Posted,
-    
   })
   console.log(data);
 
   return (
     <div>
-      {data?.map((e) => {
+      {data.map((e) => {
         const { title, views } = e;
-        return(
-        <div key={e.id}>
-            <div>title : {title}</div>
-            <div>views : : {views}</div>
-        </div>
+        return (
+          <div key={e.id}>
+            <div>Title : {title}</div>
+            <div>views : {views}</div>
+          </div>
         )
-      })}
-
+       })}
     </div>
   );
 }                             
